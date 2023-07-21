51f030e64832a949fc1d63c34235eaf04b3f748e
kir4che <mollydcxxiii@gmail.com>
2023-07-22 02:29:25 +0800

    first commit

---
+++ src/main/java/com/kir4che/movies/MovieController.java
@@ -0,0 +1,32 @@
+package com.kir4che.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+// 把 class 變成一個 bean = @Controller + @ResponseBody
+@RestController
+// 將 url 路徑對應到方法上
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    // controller 調 service 層
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(service.findMovieByImdbId(imdbId), HttpStatus.OK);
+    }
+}
